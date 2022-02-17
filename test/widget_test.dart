d3ced98a7d7de077334330d4fd9527e4bc8f8536
rwema3 <rwemadominik3@gmail.com>
2022-02-17 14:49:01 +0200

    Shop-App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // // gestures. You can also use WidgetTester to find ld widgets in the widget
-// tree, read text, and veri
+// tree, read text, and
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
