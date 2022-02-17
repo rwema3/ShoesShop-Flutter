bd5af647b3ce2e760604f12fc2c698c6511a72f3
rwema3 <rwemadominik3@gmail.com>
2022-02-17 16:09:19 +0200

    Shop-App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you ca
+// utility that Flutter provides. For example, you c
 //
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
