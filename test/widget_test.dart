aa8859c5f28fde9071c851669a0a70978108a780
rwema3 <rwemadominik3@gmail.com>
2022-02-17 19:05:32 +0200

    Shop-App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 // This is a basic Flutter widget test.
 
-// To perform an interaction with a widget
+// To perform an interaction
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
